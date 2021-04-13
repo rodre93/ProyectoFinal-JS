ea41f786f62b16d3a80f72de95eb277a7e501d72
Rodrigo Cabral <rodre.93@gmail.com>
2021-04-13 06:08:59 -0300

    Subida 3

--- js/app.js
+++ js/app.js
@@ -3,3 +3,10 @@ let nombre = prompt("Hola, ingresa tu nombre");
 
 
 alert("Bienvenido a StoreARG " + nombre);
+
+const hoodie1 = new rHoodies("Adidas", 65, "L", "Negro", 34)
+
+hoodie1.sumaImp();
+hoodie1.sumaEnvio();
+
+alert("El total es " + sumaEnvio());
